f61e299e74904746029cbb02eb855b66f374a96a
jke <wart,1972>
2020-10-19 02:04:21 +0200

    Skeleton of state- and interrupt-based lifting platform

--- examples/Platform/Platform.ino
+++ examples/Platform/Platform.ino
@@ -6,20 +6,36 @@
 #include </Development/StateMachine/src/StateMachine.h>
 #include "PlatformControl.h"
 
+#define MS 1L
+#define SEC  (1000*MS)
+#define MIN  (60*SEC)
+
+#define PARK_SWITCH_PIN  2
+#define TOOL_SWITCH_PIN  3
+#define PARK_BUTTON_PIN 18
+#define TOOL_BUTTON_PIN 19
+#define ZERO_BUTTON_PIN 20
+
 // Definition
 typedef enum                {PARKED,   GO_PARK,   START,   MANUAL,   STOPPED,  GO_TOOL,    TOOLING} States;
 const char* stateNames[] = {"Parked", "Go park", "Start", "Manual", "Stopped", "Go tool", "Tooling"};
 StateMachine state(7, stateNames, millis);
 
 PlatformControl platform;
+long            displayZero;  // position that should be displayed as 0.0 mm
 
 void setup() {
   // Registration
-  state.onEnter(START, enterStart);
-  state.onState(START, onStart);
-  state.onState(MANUAL, onManual);
-  state.onState(STOPPED, onStopped);
+  state.onEnter(START, onEnterStart);
+  state.onEnter(STOPPED, onEnterStopped);
+  state.onEnter(GO_TOOL, onEnterGoingTool);
   state.onState(GO_TOOL, onGoingTool);
+  state.onTimeout(GO_TOOL, 1*MIN, onTimeoutGoingTool);
+  state.onEnter(TOOLING, onEnterTooling);
+  state.onEnter(GO_PARK, onEnterGoingPark);
+  state.onState(GO_PARK, onGoingPark);
+  state.onTimeout(GO_PARK, 2*MIN, onTimeoutGoingPark);
+  state.onEnter(PARKED, onEnterParked);
 
   // Startup procedure to find start position
   if (!isParkPosition()) {
@@ -28,7 +44,12 @@ void setup() {
   platform.doUnparkBlocking(); // go to start position
   state.toState(START);
 
-  // TODO: Setup ISR
+  attachInterrupt(digitalPinToInterrupt(PARK_SWITCH_PIN), onParkSwitch, LOW);
+  attachInterrupt(digitalPinToInterrupt(TOOL_SWITCH_PIN), onToolSwitch, LOW);
+  attachInterrupt(digitalPinToInterrupt(PARK_BUTTON_PIN), onParkButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(TOOL_BUTTON_PIN), onToolButton, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ZERO_BUTTON_PIN), onZeroButton, FALLING);
+  // TODO: Setup ISR for up/down buttons 4x (pin change library)
 }
 
 void loop() {
@@ -36,76 +57,110 @@ void loop() {
   state.loop();
 }
 
-void enterStart() {
-  platform.setStartPosition();
+void onEnterStart() {
+  platform.zeroPosition();
+  displayZero = 0L;
 }
 
-void onStart() {
-  if (isUpButtonPressed() || isUpFastButtonPressed()) {
-    state.toState(MANUAL);
-  } else (isToolingButtonPressed()) {
-    state.toState(GO_TOOL);
-  } else (isParkButtonPressed()) {
-    state.toState(GO_PARK);
-  } else (isZeroButtonPressed()) {
-    platform.setStartPosition();
-  }
+void onEnterStopped() {
+  platform.stop();
 }
 
-void onManual() {
-  if (isParkPosition()) {
-    platform.doUnparkBlocking();    // go to start again
-    state.toState(START);
-  } else if (isToolPosition()) {
-    state.toState(TOOLING);         // cannot go further up
-  } else if (isUpButtonPressed()) {
-    platform.directionUp();
-    platform.setSpeedNormal();
-    platform.doMove();
-  } else if (isUpFastButtonPressed()) {
-    platform.directionUp();
-    platform.setSpeedFast();
-    platform.doMove();
-  } else if (isDownButtonPressed()) {
-    platform.directionDown();
-    platform.setSpeedNormal();
-    platform.doMove();
-  } else if (isDownFastButtonPressed()) {
-    platform.directionDown();
-    platform.setSpeedFast();
-    platform.doMove();
-  } else if (isToolingButtonPressed()) {
-    state.toState(GO_TOOL);
-  } else if (isParkButtonPressed()) {
-    state.toState(GO_PARK);
-  } else {
-    state.toState(STOPPED);
-  }
-}
-
-void onStopped() {
-  if (isUpButtonPressed() || isUpFastButtonPressed() || isDownButtonPressed() || isDownFastButtonPressed()) {
-    state.toState(MANUAL);
-  } else (isToolingButtonPressed()) {
-    state.toState(GO_TOOL);
-  } else (isParkButtonPressed()) {
-    state.toState(GO_PARK);
-  } else (isZeroButtonPressed()) {
-    platform.setStartPosition();
-  }
+void onEnterGoingTool() {
+  platform.moveTop(FAST); // approx. position known
 }
 
 void onGoingTool() {
+  if (!platform.moving()) {
+    // moved up to approx. position but tool switch not contacted
+    platform.moveUp(SLOW);
+  }
+}
+
+void onTimeoutGoingTool() {
+  // showAlarm()
+  state.toState(STOPPED);
+}
+
+void onEnterTooling() {
+  // showTooling()
+}
+
+void onEnterGoingPark() {
+  platform.movePark(FAST); // approx. position known
+}
+
+void onGoingPark() {
+  if (!platform.moving()) {
+    // moved down to approx. position but tool switch not contacted
+    platform.moveDown(SLOW);
+  }
+}
+
+void onTimeoutGoingPark() {
+  // showAlarm()
+  state.toState(STOPPED);
+}
+
+void onEnterParked() {
+  // showParked()
 }
 
 // ISR
 void onParkSwitch() { platform.stop(); state.toState(PARKED); }
 void onToolSwitch() { platform.stop(); state.toState(TOOLING); }
 
+void onParkButton() { state.toState(GO_PARK); }
+void onToolButton() { state.toState(GO_TOOL); }
+void onZeroButton() { displayZero = platform.position(); }
+
+void onUpButton() {
+  if (state.state() == TOOLING) return;
+  if (isUpButtonPressed()) {
+    platform.moveUp(SLOW);
+    state.toState(MANUAL);
+  } else {
+    state.toState(STOPPED);
+  }
+}
+
+void onUpFastButton() {
+  if (state.state() == TOOLING) return;
+  if (isUpFastButtonPressed()) {
+    platform.moveUp(FAST);
+    state.toState(MANUAL);
+  } else {
+    state.toState(STOPPED);
+  }
+}
+
+void onDownButton() {
+  if (state.state() == PARKED) return;
+  if (state.state() == START) return;
+  if (isDownButtonPressed()) {
+    platform.moveDown(SLOW);
+    state.toState(MANUAL);
+  } else {
+    state.toState(STOPPED);
+  }
+}
+
+void onDownFastButton() {
+  if (state.state() == PARKED) return;
+  if (state.state() == START) return;
+  if (isDownButtonPressed()) {
+    platform.moveDown(FAST);
+    state.toState(MANUAL);
+  } else {
+    state.toState(STOPPED);
+  }
+}
+
 bool isParkPosition() {}
 bool isToolPosition() {}
+
 bool isParkButtonPressed() {}
-bool isToolingButtonPressed() {}
+bool isToolButtonPressed() {}
 bool isUpButtonPressed() {}
 bool isUpFastButtonPressed() {}
 bool isDownButtonPressed() {}
--- examples/Platform/PlatformControl.h
+++ examples/Platform/PlatformControl.h
@@ -1,5 +1,16 @@
+#define SLOW   50   // steps/second
+#define FAST 2000   // steps/second
+
 class PlatformControl {
   public:
-    void doParkBlocking() {};
-    void doUnparkBlocking() {};
+    void doParkBlocking() {}
+    void doUnparkBlocking() {}
+    void movePark(int speed) {}
+    void moveTop(int speed) {}
+    void moveDown(int speed) {}
+    void moveUp(int speed) {}
+    void stop() {}
+    bool moving() {}
+    long position() {}
+    void zeroPosition() {}
 };
